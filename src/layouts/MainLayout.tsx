22c65f61d9e0f4cc4db6b9a2b92375fa8626320a
Prince <princekrdss2018@gmail.com>
2025-02-21 19:52:13 +0530

    everything is done

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import backgroundImage from "../assets/Code Rev Background.webp";
 
 const MainLayout = () => {
   return (
     // <div className="flex flex-col min-h-screen bg-[url('https://images.pexels.com/photos/7130469/pexels-photo-7130469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')] bg-center bg-cover bg-fixed">
-      
-    <div className="flex flex-col min-h-screen bg-[url('https://img.freepik.com/free-vector/gradient-blur-pink-blue-abstract-background_53876-117324.jpg?t=st=1740032026~exp=1740035626~hmac=569724f229e2f8e37430a5c386ffa682715538f1c6ec88b23b4d4644557df6f3&w=1380')] bg-center bg-cover bg-fixed">
-      
+
+    <div
+      className="flex flex-col min-h-screen bg-center bg-cover bg-fixed"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       {/* Header */}
       <Header />
 
